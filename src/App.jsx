199eb3d0fefcd9404f338a8867bc6e2512137144
krishgupta1 <krishgupta11110@gmail.com>
2023-05-29 20:44:59 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import Random from './components/Random'
+import Tag from './components/Tag'
+
 export default function App() {
-  return <div>Random GIF</div>;
+  return (
+    <div className="w-full flex flex-col background relative overflow-hidden items-center">
+      <h1 className="bg-white w-11/12 text-center mt-[40px] rounded-lg px-10 py-2 text-4xl font-bold">
+        RANDOM GIFS
+      </h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]  ">
+        <Random/>
+        <Tag/>
+      </div>
+    </div>
+  );
 }
